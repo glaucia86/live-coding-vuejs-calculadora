140654c3bfdc35888aaa87e1e8c8801e7dfa4aa9
glaucia86 <glaucia_lemos86@hotmail.com>
2019-05-24 23:38:03 -0300

    Início do desenvolvimento da Calculadora.

--- calculadora-app/src/App.vue
+++ calculadora-app/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculadora />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculadora from './components/Calculadora.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Calculadora
   }
 }
 </script>
---
+++ calculadora-app/src/components/Calculadora.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculadora',
+  data() {
+    return {
+      msg: 'Oi!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
