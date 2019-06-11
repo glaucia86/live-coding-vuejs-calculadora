d32b59fbcd8462aedc599f56bd9eac4e70d773ce
glaucia86 <glaucia_lemos86@hotmail.com>
2019-06-10 23:12:54 -0300

    Finalização do desenvolvimento da lógica da Calculadora.

--- calculadora-app/src/components/Calculadora.vue
+++ calculadora-app/src/components/Calculadora.vue
@@ -1,31 +1,118 @@
 <template>
   <div class="calculadora">
-    <div class="display">0</div>
-    <div class="botao">C</div>
-    <div class="botao">+/-</div>
-    <div class="botao">%</div>
-    <div class="botao operadores">÷</div>
-    <div class="botao">7</div>
-    <div class="botao">8</div>
-    <div class="botao">9</div>
-    <div class="botao operadores">x</div>
-    <div class="botao">4</div>
-    <div class="botao">5</div>
-    <div class="botao">6</div>
-    <div class="botao operadores">-</div>
-    <div class="botao">1</div>
-    <div class="botao">2</div>
-    <div class="botao">3</div>
-    <div class="botao operadores">+</div>
-    <div class="botao zero">0</div>
-    <div class="botao">.</div>
-    <div class="botao operadores">=</div>
+    <div class="display">{{valorCorrente || '0'}}</div>
+    <div v-on:click="limpar" class="botao">C</div>
+    <div v-on:click="sinal" class="botao">+/-</div>
+    <div v-on:click="porcentagem" class="botao">%</div>
+    <div v-on:click="dividir" class="botao operadores">÷</div>
+    <div v-on:click="juntarNumeros('7')" class="botao">7</div>
+    <div v-on:click="juntarNumeros('8')" class="botao">8</div>
+    <div v-on:click="juntarNumeros('9')" class="botao">9</div>
+    <div v-on:click="multiplicar" class="botao operadores">x</div>
+    <div v-on:click="juntarNumeros('4')" class="botao">4</div>
+    <div v-on:click="juntarNumeros('5')" class="botao">5</div>
+    <div v-on:click="juntarNumeros('6')" class="botao">6</div>
+    <div v-on:click="diminuir" class="botao operadores">-</div>
+    <div v-on:click="juntarNumeros('1')" class="botao">1</div>
+    <div v-on:click="juntarNumeros('2')" class="botao">2</div>
+    <div v-on:click="juntarNumeros('3')" class="botao">3</div>
+    <div v-on:click="somar" class="botao operadores">+</div>
+    <div v-on:click="juntarNumeros('0')" class="botao zero">0</div>
+    <div v-on:click="ponto" class="botao">.</div>
+    <div v-on:click="resultado" class="botao operadores">=</div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      valorCorrente: '',
+      numeroAnterior: null,
+      operador: null,
+      operadorClicado: false,
+    };
+  },
+  methods: {
+    /**
+     * Método responsável por limpar o display da 'Calculadora':
+     */
+    limpar() {
+      this.valorCorrente = '';
+    },
 
+    /**
+     * Método responsável por colocar o sinal '-' ou '+' para a realização
+     * de cálculos operacionais.
+     * - chartAt --> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+     * - slice --> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+     */
+    sinal() {
+      this.valorCorrente = this.valorCorrente.charAt(0) === '-'
+        ? this.valorCorrente.slice(1)
+        : `-${this.valorCorrente}`;
+    },
+
+    // Método responsávelpor realizar operações de 'porcentagem'
+    porcentagem() {
+      this.valorCorrente = `${parseFloat(this.valorCorrente) / 100}`;
+    },
+
+    // Método responsável por juntar os números no display da Calculadora:
+    juntarNumeros(numero) {
+      if (this.operadorClicado) {
+        this.valorCorrente = '';
+        this.operadorClicado = false;
+      }
+
+      this.valorCorrente = `${this.valorCorrente}${numero}`;
+    },
+
+    // Método responsável por adicionar 'ponto' no display da Calculadora:
+    ponto() {
+      if (this.valorCorrente.indexOf('.') === -1) {
+        this.juntarNumeros('.');
+      }
+    },
+
+    setarValor() {
+      this.numeroAnterior = this.valorCorrente;
+      this.operadorClicado = true;
+    },
+
+    // Método responsável por realizar a operação da 'divisão'
+    dividir() {
+      this.operador = (num1, num2) => num1 / num2;
+      this.setarValor();
+    },
+
+    // Método responsável por realizar a operação da 'multiplicar'
+    multiplicar() {
+      this.operador = (num1, num2) => num1 * num2;
+      this.setarValor();
+    },
+
+    // Método responsável por realizar a operação da 'diminuir'
+    diminuir() {
+      this.operador = (num1, num2) => num1 - num2;
+      this.setarValor();
+    },
+
+    // Método responsável por realizar a operação da 'somar'
+    somar() {
+      this.operador = (num1, num2) => num1 + num2;
+      this.setarValor();
+    },
+
+    // Método responsável por apresentar o resultado das operações da Calculadora:
+    resultado() {
+      this.valorCorrente = `${this.operador(
+        parseFloat(this.numeroAnterior),
+        parseFloat(this.valorCorrente),
+      )}`;
+      this.numeroAnterior = null;
+    },
+  },
 };
 </script>
 
