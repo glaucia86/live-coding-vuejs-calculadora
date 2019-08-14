482dbccebf515d25952cb720f102fb3bdf443d49
glaucia86 <glaucia_lemos86@hotmail.com>
2019-08-13 21:50:52 -0300

    Alteração na arquitetura do projeto - split file component

--- calculadora-app/src/components/Calculadora/Calculadora.js
+++ calculadora-app/src/components/Calculadora/Calculadora.js
@@ -1,29 +1,10 @@
-<template>
-  <div class="calculadora">
-    <div class="display">{{valorCorrente || '0'}}</div>
-    <div v-on:click="limpar" class="botao">C</div>
-    <div v-on:click="sinal" class="botao">+/-</div>
-    <div v-on:click="porcentagem" class="botao">%</div>
-    <div v-on:click="dividir" class="botao operadores">÷</div>
-    <div v-on:click="juntarNumeros('7')" class="botao">7</div>
-    <div v-on:click="juntarNumeros('8')" class="botao">8</div>
-    <div v-on:click="juntarNumeros('9')" class="botao">9</div>
-    <div v-on:click="multiplicar" class="botao operadores">x</div>
-    <div v-on:click="juntarNumeros('4')" class="botao">4</div>
-    <div v-on:click="juntarNumeros('5')" class="botao">5</div>
-    <div v-on:click="juntarNumeros('6')" class="botao">6</div>
-    <div v-on:click="diminuir" class="botao operadores">-</div>
-    <div v-on:click="juntarNumeros('1')" class="botao">1</div>
-    <div v-on:click="juntarNumeros('2')" class="botao">2</div>
-    <div v-on:click="juntarNumeros('3')" class="botao">3</div>
-    <div v-on:click="somar" class="botao operadores">+</div>
-    <div v-on:click="juntarNumeros('0')" class="botao zero">0</div>
-    <div v-on:click="ponto" class="botao">.</div>
-    <div v-on:click="resultado" class="botao operadores">=</div>
-  </div>
-</template>
+/**
+ * Arquivo: src/components/Calculadura.js
+ * Data: 13/08/2019
+ * Author: Glaucia Lemos
+ * Descrição: arquivo responsável por tratar toda a lógica da Calculadora.
+ */
 
-<script>
 export default {
   data() {
     return {
@@ -44,8 +25,8 @@ export default {
     /**
      * Método responsável por colocar o sinal '-' ou '+' para a realização
      * de cálculos operacionais.
-     * - chartAt --> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
-     * - slice --> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+     * chartAt --> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+     * slice --> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
      */
     sinal() {
       this.valorCorrente = this.valorCorrente.charAt(0) === '-'
@@ -114,37 +95,3 @@ export default {
     },
   },
 };
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-.calculadora {
-  margin: 0 auto;
-  width: 350px;
-  font-size: 40px;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: minmax(50px, auto);
-}
-
-.display {
-  grid-column: 1 / 5;
-  background-color: #333;
-  color: white;
-}
-
-.zero {
-  grid-column: 1 / 3;
-}
-
-.botao {
-  background-color: #f2f2f2;
-  border: 1px solid #999;
-}
-
-.operadores {
-  background-color: orange;
-  color: white;
-}
-</style>
---
+++ calculadora-app/src/components/Calculadora/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="calculadora">
+    <div class="display">{{valorCorrente || '0'}}</div>
+    <div v-on:click="limpar" class="botao">C</div>
+    <div v-on:click="sinal" class="botao">+/-</div>
+    <div v-on:click="porcentagem" class="botao">%</div>
+    <div v-on:click="dividir" class="botao operadores">÷</div>
+    <div v-on:click="juntarNumeros('7')" class="botao">7</div>
+    <div v-on:click="juntarNumeros('8')" class="botao">8</div>
+    <div v-on:click="juntarNumeros('9')" class="botao">9</div>
+    <div v-on:click="multiplicar" class="botao operadores">x</div>
+    <div v-on:click="juntarNumeros('4')" class="botao">4</div>
+    <div v-on:click="juntarNumeros('5')" class="botao">5</div>
+    <div v-on:click="juntarNumeros('6')" class="botao">6</div>
+    <div v-on:click="diminuir" class="botao operadores">-</div>
+    <div v-on:click="juntarNumeros('1')" class="botao">1</div>
+    <div v-on:click="juntarNumeros('2')" class="botao">2</div>
+    <div v-on:click="juntarNumeros('3')" class="botao">3</div>
+    <div v-on:click="somar" class="botao operadores">+</div>
+    <div v-on:click="juntarNumeros('0')" class="botao zero">0</div>
+    <div v-on:click="ponto" class="botao">.</div>
+    <div v-on:click="resultado" class="botao operadores">=</div>
+  </div>
+</template>
+
+<script src="./Calculadora.js"></script>
+
+<style src="./calculadora.css"></style>
